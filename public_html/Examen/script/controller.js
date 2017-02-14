88578ba7432a872727d2a5a85861e169ac50ed6d
Julian <julian.egea.ponce@gmail.com>
2017-02-14 19:51:38 +0100

    Exam done.

--- public_html/Examen/script/controller.js
+++ public_html/Examen/script/controller.js
@@ -3,11 +3,11 @@ app.controller('teamsPlayersController', ['$scope', 'myService',
     function ($scope, myService) {
 
         $scope.tsm = myService.newTeam('Team SoloMid', 17, 1, "League of Legends");
-        $scope.p1 = myService.newP(0, "Hauntzer", "Kevin Yarnell", "Top Laner", 2.29);
-        $scope.p2 = myService.newP(0, "Svenskeren", "Dennis Johnsen", "Jungler", 3.78);
-        $scope.p3 = myService.newP(0, "Bjergsen", "Søren Bjerg", "Mid Laner", 4.38);
-        $scope.p4 = myService.newP(0, "WildTurtle", "Jason Tran", "AD Carry", 3.89);
-        $scope.p5 = myService.newP(0, "Biofrost", "Vincent Wang", "Support", 1.10);
+        $scope.p1 = myService.newP(0, "Hauntzer", "Kevin Yarnell", "Top Laner", 2.29, 130);
+        $scope.p2 = myService.newP(0, "Svenskeren", "Dennis Johnsen", "Jungler", 3.78, 60);
+        $scope.p3 = myService.newP(0, "Bjergsen", "Søren Bjerg", "Mid Laner", 4.38, 130);
+        $scope.p4 = myService.newP(0, "WildTurtle", "Jason Tran", "AD Carry", 3.89, 130);
+        $scope.p5 = myService.newP(0, "Biofrost", "Vincent Wang", "Support", 1.10, 125);
 
         $scope.teamName = $scope.teamName;
         $scope.teamVictories = $scope.teamVictories;
@@ -18,20 +18,57 @@ app.controller('teamsPlayersController', ['$scope', 'myService',
         $scope.playerName = $scope.playerName;
         $scope.playerRole = $scope.playerRole;
         $scope.playerKda = $scope.playerKda;
+        $scope.playerMinutes = $scope.playerMinutes;
 
         $scope.teamSubFunc = function () {
             $scope.team = myService.newTeam($scope.teamName, $scope.teamVictories, $scope.teamDefeats, $scope.game);
             console.log("Submit de teams");
         }
         $scope.playerSubFunc = function (selectedTeam) {
-            $scope.player = myService.newP(selectedTeam, $scope.playerNick, $scope.playerName, $scope.playerRole, $scope.playerKda);
-            console.log("Submit de players");
+            var found = false;
+            for (var i = 0; i < $scope.teams[selectedTeam].players.length; i++) {
+                console.log($scope.teams[selectedTeam.players]);
+                if ($scope.teams[selectedTeam].players[i].nickname == $scope.playerNick) {
+                    found = true;
+                    $scope.teams[selectedTeam].players[i].updatePlayer($scope.playerNick, $scope.playerRole, $scope.playerKda, $scope.playerMinutes);
+                }
+            }
+            if(!found){
+                $scope.player = myService.newP(selectedTeam, $scope.playerNick, $scope.playerName, $scope.playerRole, $scope.playerKda, $scope.playerMinutes);
+                console.log("Submit de players");
+            }
         }
 
         $scope.teams = myService.getAllTeams();
         $scope.players = myService.getAllPlayers();
+        $scope.tournaments = myService.getAllTournaments();
 
         $scope.deletePlayerFunc = function (selectedTeam, playerIndex) {
             $scope.teams[selectedTeam].delPlayer(playerIndex);
         }
+        $scope.modifyPlayerFunc = function (teamIndex, playerIndex) {
+            $scope.playerNick = $scope.teams[teamIndex].players[playerIndex].nickname;
+            $scope.playerName = $scope.teams[teamIndex].players[playerIndex].name;
+            $scope.playerRole = $scope.teams[teamIndex].players[playerIndex].role;
+            $scope.playerKda = $scope.teams[teamIndex].players[playerIndex].kda;
+            $scope.playerMinutes = $scope.teams[teamIndex].players[playerIndex].minutes;
+        }
+
+        //-----------EXMAEN-----------
+        $scope.tour1 = myService.newTournament(0, 1, "2017");
+        $scope.tour2 = myService.newTournament(0, 1, "2016");
+        $scope.tour3 = myService.newTournament(0, 1, "2015");
+        $scope.tour4 = myService.newTournament(0, 1, "2014");
+
+        $scope.firstLastTourFunc = function (selectedTeam) {
+            $scope.teams[selectedTeam].firstLast();
+        }
+        $scope.removeTourFunc = function (year, team) {
+            for (var i = 0; i < $scope.tournaments.length; i++) {
+                if ($scope.tournaments[i].year == year) {
+                    $scope.teams[team].removeTournament(year);
+                }
+            }
+        }
+        //-----------EXMAEN-----------
     }]);
--- public_html/Examen/script/service.js
+++ public_html/Examen/script/service.js
@@ -1,15 +1,16 @@
-function Player(nick, name, role, kda) {
+function Player(nick, name, role, kda, minutes) {
     this.nickname = nick;
     this.name = name;
     this.role = role;
     this.kda = kda;
+    this.minutes = minutes;
 
-    this.updatePlayer = function (nick, role, kda) {
+    this.updatePlayer = function (nick, role, kda, minutes) {
         this.nickname = nick;
         this.role = role;
         this.kda = kda;
+        this.minutes = minutes;
     }
-
 }
 
 function Team(name, victories, defeats, game) {
@@ -18,9 +19,10 @@ function Team(name, victories, defeats, game) {
     this.defeats = defeats;
     this.game = game;
     this.players = [];
+    this.tournaments = [];
 
     this.addPlayer = function (player) {
-        this.players.push(player)
+        this.players.push(player);
     }
     this.getPlayer = function (nick) {
         for (var i = 0; i < this.players.length; i++) {
@@ -29,6 +31,28 @@ function Team(name, victories, defeats, game) {
             }
         }
     }
+    this.addTournament = function (tournament){
+        this.tournaments.push(tournament);
+    }
+    
+    this.getTournaments = function (){
+        return this.tournaments;
+    }
+    
+    this.firstLast = function (){
+        var firstTournament = this.tournaments[0];
+        this.tournaments.shift();
+        this.tournaments.push(firstTournament);
+    }
+    
+    this.removeTournament = function (year){
+        for (var i = 0; i < this.tournaments.length; i++) {
+            if (this.tournaments[i].year == year) {
+                this.tournaments.splice(i, 1);
+            }
+        }
+    }
+    
     this.delPlayer = function (playerIndex) {
         this.players.splice(playerIndex, 1);
     }
@@ -45,17 +69,23 @@ function Team(name, victories, defeats, game) {
     }
 }
 
+function Tournament(position, year){
+    this.position = position;
+    this.year = year;
+}
+
 app.service('myService', function () {
     
     this.players = new Array();
     this.teams = new Array();
+    this.tournaments = new Array();
     
     this.newPlayer = function (nick, name, role, kda) {
         var player = new Player(nick, name, role, kda);
         this.players.push(player);
     }
-    this.newP = function (team, nick, name, role, kda) {
-        var player = new Player(nick, name, role, kda);
+    this.newP = function (team, nick, name, role, kda, minutes) {
+        var player = new Player(nick, name, role, kda, minutes);
         this.teams[team].players.push(player);
         this.players.push(player);
     }
@@ -63,10 +93,18 @@ app.service('myService', function () {
         var team = new Team(name, victories, defeats, game);
         this.teams.push(team);
     }
+    this.newTournament = function (team, position, year){
+        var tournament = new Tournament(position, year);
+        this.teams[team].tournaments.push(tournament);
+        this.tournaments.push(tournament);
+    }
     this.getAllPlayers = function(){
         return this.players;
     }
     this.getAllTeams =  function(){
         return this.teams;
+    }
+    this.getAllTournaments = function (){
+        return this.tournaments;
     }
 });
